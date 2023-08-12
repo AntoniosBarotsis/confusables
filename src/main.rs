7accd4f42a42712d34a5f6892652ff2bb0370f01
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2023-08-12 20:22:41 +0300

    testing some arabic

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 include!(concat!(env!("OUT_DIR"), "/codegen.rs"));
 
 fn main() {
-  let c = "꓾";
+  let c = "صلى الله عليه وسلم";
+  // let c = "꓾";
   let tmp = KEYWORDS.get(c).expect("So confusing");
+
   println!("{c} -> {tmp}");
 }
