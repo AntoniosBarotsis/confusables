79536d0d2ddcb93e7ae377777b4168e48ca05b5a
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2023-08-12 20:23:01 +0300

    Simplified code and support RTL langs

--- build.rs
+++ build.rs
@@ -5,6 +5,8 @@ use std::fs::File;
 use std::io::{BufWriter, Write};
 use std::path::Path;
 
+use phf_codegen::Map;
+
 fn main() {
   // Run the build script when either this or the data file change
   println!("cargo:rerun-if-changed=build.rs");
@@ -25,38 +27,16 @@ fn main() {
     .collect::<Vec<_>>();
 
   for el in data {
-    // Add a tab at the end of the string so we always read until a tab
-    let tmp = el + "\t";
-    let chars = tmp.chars();
+    let first_char = el.chars().next().unwrap();
 
-    // Some confusables are multiple characters so we need to accumulate them.
-    // They are separated by tabs.
-    let mut accumulator = String::new();
-    let mut first = String::new();
-    let mut first_parsed = false;
-
-    for c in chars {
-      if !first_parsed {
-        if c == '\t' {
-          first_parsed = true;
-        } else {
-          first += &c.to_string();
-        }
-        continue;
-      }
-
-      // Delimiter
-      if c == '\t' {
-        let _ = map.entry(accumulator, &format!("r\"{first}\""));
-
-        accumulator = String::new();
-        first = String::new();
-        continue;
-      }
-
-      // Append
-      accumulator += &(c.clone()).to_string();
+    if is_rtl(first_char) {
+      yes(el.rsplit_terminator('\t'), &mut map);
+    } else {
+      yes(el.split_terminator('\t'), &mut map);
     }
+
+    // let mut confusables = el.split_terminator('\t');
+    // let first = confusables.next().unwrap();
   }
 
   write!(
@@ -68,4 +48,42 @@ fn main() {
   .unwrap();
 
   writeln!(&mut file, ";").unwrap();
+}
+
+// The suggested lint fix doesn't compile 0_0
+#[allow(single_use_lifetimes)]
+fn yes<'a>(mut confusables: impl Iterator<Item = &'a str>, map: &mut Map<String>) {
+  let first = confusables.next().unwrap();
+
+  for c in confusables {
+    let _ = map.entry(c.to_owned(), &format!("r\"{first}\""));
+  }
+}
+
+/// This returns true if the character belongs in a right-to-left (RTL) alphabet
+///
+/// The unicode ranges used in this function were taken from
+/// [here](https://www.unicodepedia.com/groups/) and the list of RTL alphabets were taken
+/// from [here](https://www.worldatlas.com/articles/which-languages-are-written-from-right-to-left.html).
+///
+/// ## Unicode Groups
+///
+/// Not all the RTL alphabets seem to be represented in the unicode standard so these are the ones
+/// the code *actually* looks for:
+///
+/// - Arabic
+/// - Imperial Aramaic
+/// - Hebrew
+/// - Old Persian
+/// - Syrian
+// TODO: Move this to a separate crate and publish it as it might actually be useful
+const fn is_rtl(c: char) -> bool {
+  match c {
+    '\u{600}'..='\u{6FF}'|         // Arabic
+    '\u{10840}'..='\u{1085F}' |    // Imperial Aramaic
+    '\u{591}'..='\u{5F4}'|         // Hebrew
+    '\u{103A0}'..='\u{103D5}' |    // Old Persian
+    '\u{700}'..='\u{74F}' => true, // Syrian
+    _ => false
+  }
 }
