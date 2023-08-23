a9b00cedc59a771683f807d9e3e035b02bf7a8a7
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2023-08-23 20:18:51 +0300

    Did some stuff

--- crates/confusables-js/src/lib.rs
+++ crates/confusables-js/src/lib.rs
@@ -1,19 +1,46 @@
 #![allow(clippy::unnecessary_wraps)]
 
+use confusables::Confusable;
 use neon::prelude::*;
 
-fn hello(mut cx: FunctionContext<'_>) -> JsResult<'_, JsString> {
-  Ok(cx.string("hello node"))
+fn contains_confusable(mut cx: FunctionContext<'_>) -> JsResult<'_, JsBoolean> {
+  let text = cx.argument::<JsString>(0)?.value(&mut cx);
+
+  Ok(cx.boolean(text.contains_confusable()))
 }
 
-fn hello_name(mut cx: FunctionContext<'_>) -> JsResult<'_, JsString> {
-  let name = cx.argument::<JsString>(0)?.value(&mut cx);
-  Ok(cx.string(format!("hello {name}")))
+fn is_confusable_with(mut cx: FunctionContext<'_>) -> JsResult<'_, JsBoolean> {
+  let left = cx.argument::<JsString>(0)?.value(&mut cx);
+  let right = cx.argument::<JsString>(1)?.value(&mut cx);
+
+  Ok(cx.boolean(left.is_confusable_with(&right)))
+}
+
+fn confusable_contains(mut cx: FunctionContext<'_>) -> JsResult<'_, JsBoolean> {
+  let left = cx.argument::<JsString>(0)?.value(&mut cx);
+  let right = cx.argument::<JsString>(1)?.value(&mut cx);
+
+  Ok(cx.boolean(left.confusable_contains(&right)))
+}
+
+fn replace_confusable(mut cx: FunctionContext<'_>) -> JsResult<'_, JsString> {
+  let text = cx.argument::<JsString>(0)?.value(&mut cx);
+
+  Ok(cx.string(text.replace_confusable()))
+}
+
+fn detect_replace_confusable(mut cx: FunctionContext<'_>) -> JsResult<'_, JsString> {
+  let text = cx.argument::<JsString>(0)?.value(&mut cx);
+
+  Ok(cx.string(text.detect_replace_confusable()))
 }
 
 #[neon::main]
 fn main(mut cx: ModuleContext<'_>) -> NeonResult<()> {
-  cx.export_function("hello", hello)?;
-  cx.export_function("hello_name", hello_name)?;
+  cx.export_function("contains_confusable", contains_confusable)?;
+  cx.export_function("is_confusable_with", is_confusable_with)?;
+  cx.export_function("confusable_contains", confusable_contains)?;
+  cx.export_function("replace_confusable", replace_confusable)?;
+  cx.export_function("detect_replace_confusable", detect_replace_confusable)?;
   Ok(())
 }
