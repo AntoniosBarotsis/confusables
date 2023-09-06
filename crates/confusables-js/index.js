79a41806a0a9af5afd48e4e7bd3979589c286a5b
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2023-09-06 21:48:44 +0200

    Outdated bin name

--- crates/confusables-js/index.js
+++ crates/confusables-js/index.js
@@ -32,24 +32,24 @@ switch (platform) {
   case 'android':
     switch (arch) {
       case 'arm64':
-        localFileExisted = existsSync(join(__dirname, 'confusables-js-2.android-arm64.node'))
+        localFileExisted = existsSync(join(__dirname, 'confusables-js.android-arm64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./confusables-js-2.android-arm64.node')
+            nativeBinding = require('./confusables-js.android-arm64.node')
           } else {
-            nativeBinding = require('confusables-js-2-android-arm64')
+            nativeBinding = require('confusables-js-android-arm64')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'arm':
-        localFileExisted = existsSync(join(__dirname, 'confusables-js-2.android-arm-eabi.node'))
+        localFileExisted = existsSync(join(__dirname, 'confusables-js.android-arm-eabi.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./confusables-js-2.android-arm-eabi.node')
+            nativeBinding = require('./confusables-js.android-arm-eabi.node')
           } else {
-            nativeBinding = require('confusables-js-2-android-arm-eabi')
+            nativeBinding = require('confusables-js-android-arm-eabi')
           }
         } catch (e) {
           loadError = e
@@ -63,13 +63,13 @@ switch (platform) {
     switch (arch) {
       case 'x64':
         localFileExisted = existsSync(
-          join(__dirname, 'confusables-js-2.win32-x64-msvc.node')
+          join(__dirname, 'confusables-js.win32-x64-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./confusables-js-2.win32-x64-msvc.node')
+            nativeBinding = require('./confusables-js.win32-x64-msvc.node')
           } else {
-            nativeBinding = require('confusables-js-2-win32-x64-msvc')
+            nativeBinding = require('confusables-js-win32-x64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -77,13 +77,13 @@ switch (platform) {
         break
       case 'ia32':
         localFileExisted = existsSync(
-          join(__dirname, 'confusables-js-2.win32-ia32-msvc.node')
+          join(__dirname, 'confusables-js.win32-ia32-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./confusables-js-2.win32-ia32-msvc.node')
+            nativeBinding = require('./confusables-js.win32-ia32-msvc.node')
           } else {
-            nativeBinding = require('confusables-js-2-win32-ia32-msvc')
+            nativeBinding = require('confusables-js-win32-ia32-msvc')
           }
         } catch (e) {
           loadError = e
@@ -91,13 +91,13 @@ switch (platform) {
         break
       case 'arm64':
         localFileExisted = existsSync(
-          join(__dirname, 'confusables-js-2.win32-arm64-msvc.node')
+          join(__dirname, 'confusables-js.win32-arm64-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./confusables-js-2.win32-arm64-msvc.node')
+            nativeBinding = require('./confusables-js.win32-arm64-msvc.node')
           } else {
-            nativeBinding = require('confusables-js-2-win32-arm64-msvc')
+            nativeBinding = require('confusables-js-win32-arm64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -108,23 +108,23 @@ switch (platform) {
     }
     break
   case 'darwin':
-    localFileExisted = existsSync(join(__dirname, 'confusables-js-2.darwin-universal.node'))
+    localFileExisted = existsSync(join(__dirname, 'confusables-js.darwin-universal.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./confusables-js-2.darwin-universal.node')
+        nativeBinding = require('./confusables-js.darwin-universal.node')
       } else {
-        nativeBinding = require('confusables-js-2-darwin-universal')
+        nativeBinding = require('confusables-js-darwin-universal')
       }
       break
     } catch {}
     switch (arch) {
       case 'x64':
-        localFileExisted = existsSync(join(__dirname, 'confusables-js-2.darwin-x64.node'))
+        localFileExisted = existsSync(join(__dirname, 'confusables-js.darwin-x64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./confusables-js-2.darwin-x64.node')
+            nativeBinding = require('./confusables-js.darwin-x64.node')
           } else {
-            nativeBinding = require('confusables-js-2-darwin-x64')
+            nativeBinding = require('confusables-js-darwin-x64')
           }
         } catch (e) {
           loadError = e
@@ -132,13 +132,13 @@ switch (platform) {
         break
       case 'arm64':
         localFileExisted = existsSync(
-          join(__dirname, 'confusables-js-2.darwin-arm64.node')
+          join(__dirname, 'confusables-js.darwin-arm64.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./confusables-js-2.darwin-arm64.node')
+            nativeBinding = require('./confusables-js.darwin-arm64.node')
           } else {
-            nativeBinding = require('confusables-js-2-darwin-arm64')
+            nativeBinding = require('confusables-js-darwin-arm64')
           }
         } catch (e) {
           loadError = e
@@ -152,12 +152,12 @@ switch (platform) {
     if (arch !== 'x64') {
       throw new Error(`Unsupported architecture on FreeBSD: ${arch}`)
     }
-    localFileExisted = existsSync(join(__dirname, 'confusables-js-2.freebsd-x64.node'))
+    localFileExisted = existsSync(join(__dirname, 'confusables-js.freebsd-x64.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./confusables-js-2.freebsd-x64.node')
+        nativeBinding = require('./confusables-js.freebsd-x64.node')
       } else {
-        nativeBinding = require('confusables-js-2-freebsd-x64')
+        nativeBinding = require('confusables-js-freebsd-x64')
       }
     } catch (e) {
       loadError = e
@@ -168,26 +168,26 @@ switch (platform) {
       case 'x64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'confusables-js-2.linux-x64-musl.node')
+            join(__dirname, 'confusables-js.linux-x64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./confusables-js-2.linux-x64-musl.node')
+              nativeBinding = require('./confusables-js.linux-x64-musl.node')
             } else {
-              nativeBinding = require('confusables-js-2-linux-x64-musl')
+              nativeBinding = require('confusables-js-linux-x64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'confusables-js-2.linux-x64-gnu.node')
+            join(__dirname, 'confusables-js.linux-x64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./confusables-js-2.linux-x64-gnu.node')
+              nativeBinding = require('./confusables-js.linux-x64-gnu.node')
             } else {
-              nativeBinding = require('confusables-js-2-linux-x64-gnu')
+              nativeBinding = require('confusables-js-linux-x64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -197,26 +197,26 @@ switch (platform) {
       case 'arm64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'confusables-js-2.linux-arm64-musl.node')
+            join(__dirname, 'confusables-js.linux-arm64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./confusables-js-2.linux-arm64-musl.node')
+              nativeBinding = require('./confusables-js.linux-arm64-musl.node')
             } else {
-              nativeBinding = require('confusables-js-2-linux-arm64-musl')
+              nativeBinding = require('confusables-js-linux-arm64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'confusables-js-2.linux-arm64-gnu.node')
+            join(__dirname, 'confusables-js.linux-arm64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./confusables-js-2.linux-arm64-gnu.node')
+              nativeBinding = require('./confusables-js.linux-arm64-gnu.node')
             } else {
-              nativeBinding = require('confusables-js-2-linux-arm64-gnu')
+              nativeBinding = require('confusables-js-linux-arm64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -225,13 +225,13 @@ switch (platform) {
         break
       case 'arm':
         localFileExisted = existsSync(
-          join(__dirname, 'confusables-js-2.linux-arm-gnueabihf.node')
+          join(__dirname, 'confusables-js.linux-arm-gnueabihf.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./confusables-js-2.linux-arm-gnueabihf.node')
+            nativeBinding = require('./confusables-js.linux-arm-gnueabihf.node')
           } else {
-            nativeBinding = require('confusables-js-2-linux-arm-gnueabihf')
+            nativeBinding = require('confusables-js-linux-arm-gnueabihf')
           }
         } catch (e) {
           loadError = e
