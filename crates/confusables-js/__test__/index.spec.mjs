06e49d5ba693542145c57b6b403d0d473c66756e
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2023-09-06 21:27:52 +0200

    Move tests

--- crates/confusables-js/__test__/index.spec.mjs
+++ crates/confusables-js/__test__/index.spec.mjs
@@ -1,7 +1,31 @@
 import test from 'ava'
 
-import { sum } from '../index.js'
+import {
+  containsConfusable,
+  isConfusableWith,
+  confusableContains,
+  replaceConfusable,
+  detectReplaceConfusable,
+} from '../index.js'
+ 
+let a = "𝓗℮𝐥1೦"
+let b = "Hello"
 
-test('sum from native', (t) => {
-  t.is(sum(1, 2), 3)
+test('containsConfusable', (t) => {
+  t.true(containsConfusable(a))
+  t.true(!containsConfusable(b))
+})
+
+test('isConfusableWith', (t) => {
+  t.true(isConfusableWith(a, b))
+  t.true(!isConfusableWith(b, a))
+})
+
+test('confusableContains', (t) => {
+  t.true(confusableContains(a, b))
+})
+
+test('replaceConfusable', (t) => {
+  t.is(replaceConfusable(a), b)
+  t.is(detectReplaceConfusable(a), b)
 })
